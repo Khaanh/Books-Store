3e5ce11e33ac46a4f654799efa8178d5fd663418
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-05-29 20:15:49 +0200

    describe class Book

---
+++ domain/Books-Store/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Books_Store
+{
+    public class Book
+    {
+        public Book(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+
+
+    }
+}

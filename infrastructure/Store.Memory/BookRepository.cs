fe1561d8dcaeb11fa68132c1c5d5dc34ecbb623e
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-04 22:24:07 +0200

    Implement IBookRepository & added LINQ

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Books_Store;
 
 namespace Store.Memory
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
